239911d14abca495718876908a5404f26057d88e
cursoeuskal <cursoeuskal@gmail.com>
2025-07-25 10:32:45 +0000

    Cambio en el titulo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "FirebaseNotes",
+  title: "FirebaseNotesIA",
   description: "A simple note taking app using Firebase and Next.js",
 };
 
